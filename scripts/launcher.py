549249e9ab9c1da7f0eb673e8abf971d6f720080
Piet De Vaere <piet.de.vaere@inf.ethz.ch>
2019-10-25 13:45:22 +0200

    now allowing client to run for two minutes

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -12,6 +12,7 @@ revoke = ' --revoke' if j['revoke'] else ''
 cmd = f"{j['challenge_type']} --dir {j['dir']} --record {j['record']} {' '.join(['--domain ' + d for d in j['domains']])} {revoke}"
 dir_path = Path(__file__).absolute()
 script = dir_path.parent.parent / "project/run"
-cmd = f"timeout -k 1m 1m {script} {cmd}"
+cmd = f"timeout -k 1m 2m {script} {cmd}"
 print("Going to run:", cmd)
 p = subprocess.run(shlex.split(cmd))
+
